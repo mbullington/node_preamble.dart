54a1e496431bcf15313a726e1fbe0180281eb778
Michael Bullington <mikebullingtn@gmail.com>
2018-06-17 22:00:44 -0500

    Add support for Browserify/Webpack/browser environments.

--- lib/preamble.dart
+++ lib/preamble.dart
@@ -1,24 +1,18 @@
 library node_preamble;
 
-final _minified = r"""var self=Object.create(global);self.location={get href(){return"file://"+(e=process.cwd(),"win32"!=process.platform?e:"/"+e.replace(/\\/g,"/"))+"/";var e}},self.scheduleImmediate=setImmediate,self.require=require,self.exports=exports,self.process=process,self.__dirname=__dirname,self.__filename=__filename,function(){var e=null;self.document={get currentScript(){return null==e&&(e={src:function(){try{throw new Error}catch(a){var e=a.stack,r=new RegExp("^ *at [^(]*\\((.*):[0-9]*:[0-9]*\\)$","mg"),l=null;do{var t=r.exec(e);null!=t&&(l=t)}while(null!=t);return l[1]}}()}),e}}}(),self.dartDeferredLibraryLoader=function(e,r,l){try{load(e),r()}catch(e){l(e)}};""";
+final _minified = r"""var self=Object.create(global);self.scheduleImmediate=self.setImmediate?self.setImmediate:function(e){setTimeout(e,0)},self.require=require,self.exports=exports,self.process=process,self.__dirname=__dirname,self.__filename=__filename,global.window||(self.location={get href(){return"file://"+(e=process.cwd(),"win32"!=process.platform?e:"/"+e.replace(/\\/g,"/"))+"/";var e}},function(){var e=null;self.document={get currentScript(){return null==e&&(e={src:function(){try{throw new Error}catch(s){var e=s.stack,r=new RegExp("^ *at [^(]*\\((.*):[0-9]*:[0-9]*\\)$","mg"),l=null;do{var t=r.exec(e);null!=t&&(l=t)}while(null!=t);return l[1]}}()}),e}}}(),self.dartDeferredLibraryLoader=function(e,r,l){try{load(e),r()}catch(e){l(e)}});""";
 
 final _normal = r"""
+// make sure to keep this as 'var'
+// we don't want block scoping
 var self = Object.create(global);
 
-// TODO: This isn't really a correct transformation. For example, it will fail
-// for paths that contain characters that need to be escaped in URLs. Once
-// dart-lang/sdk#27979 is fixed, it should be possible to make it better.
-self.location = {
-  get href() {
-    return "file://" + (function() {
-      var cwd = process.cwd();
-      if (process.platform != "win32") return cwd;
-      return "/" + cwd.replace(/\\/g, "/");
-    })() + "/";
-  }
-};
+self.scheduleImmediate = self.setImmediate
+    ? self.setImmediate
+    : function(cb) {
+        setTimeout(cb, 0);
+      };
 
-self.scheduleImmediate = setImmediate;
 self.require = require;
 self.exports = exports;
 self.process = process;
@@ -26,42 +20,58 @@ self.process = process;
 self.__dirname = __dirname;
 self.__filename = __filename;
 
-(function() {
-  function computeCurrentScript() {
-    try {
-      throw new Error();
-    } catch(e) {
-      var stack = e.stack;
-      var re = new RegExp("^ *at [^(]*\\((.*):[0-9]*:[0-9]*\\)$", "mg");
-      var lastMatch = null;
-      do {
-        var match = re.exec(stack);
-        if (match != null) lastMatch = match;
-      } while (match != null);
-      return lastMatch[1];
-    }
-  }
-
-  var cachedCurrentScript = null;
-  self.document = {
-    get currentScript() {
-      if (cachedCurrentScript == null) {
-        cachedCurrentScript = {src: computeCurrentScript()};
-      }
-      return cachedCurrentScript;
+// if we're running in a browser, Dart supports most of this out of box
+// make sure we only run these in Node.js environment
+if (!global.window) {
+  // TODO: This isn't really a correct transformation. For example, it will fail
+  // for paths that contain characters that need to be escaped in URLs. Once
+  // dart-lang/sdk#27979 is fixed, it should be possible to make it better.
+  self.location = {
+    get href() {
+      return "file://" + (function() {
+        var cwd = process.cwd();
+        if (process.platform != "win32") return cwd;
+        return "/" + cwd.replace(/\\/g, "/");
+      })() + "/";
     }
   };
-})();
 
-self.dartDeferredLibraryLoader = function(uri, successCallback, errorCallback) {
-  try {
-    load(uri);
-    successCallback();
-  } catch (error) {
-    errorCallback(error);
-  }
-};
-""";
+  (function() {
+    function computeCurrentScript() {
+      try {
+        throw new Error();
+      } catch(e) {
+        var stack = e.stack;
+        var re = new RegExp("^ *at [^(]*\\((.*):[0-9]*:[0-9]*\\)$", "mg");
+        var lastMatch = null;
+        do {
+          var match = re.exec(stack);
+          if (match != null) lastMatch = match;
+        } while (match != null);
+        return lastMatch[1];
+      }
+    }
+
+    var cachedCurrentScript = null;
+    self.document = {
+      get currentScript() {
+        if (cachedCurrentScript == null) {
+          cachedCurrentScript = {src: computeCurrentScript()};
+        }
+        return cachedCurrentScript;
+      }
+    };
+  })();
+
+  self.dartDeferredLibraryLoader = function(uri, successCallback, errorCallback) {
+    try {
+     load(uri);
+      successCallback();
+    } catch (error) {
+      errorCallback(error);
+    }
+  };
+}""";
 
 /// Returns the text of the preamble.
 ///
